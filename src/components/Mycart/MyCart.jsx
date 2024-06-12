7db923eb7285f850ebc4405b5ca30810e97375cd
ibrahim75 <ibrahimbiplob75@gmail.com>
2024-06-12 20:31:20 +0600

    Product cart see more button implemented

--- src/components/Mycart/MyCart.jsx
+++ src/components/Mycart/MyCart.jsx
@@ -6,6 +6,8 @@ const MyCart = () => {
     const [cartItem,setCartItem]=useState()
     const [nodata,setNodata]=useState()
 
+    const [see,setSee]=useState(false)
+
     useEffect(()=>{
         const cart_item = JSON.parse(localStorage.getItem("cart"));
         if(cart_item){
@@ -31,21 +33,39 @@ const MyCart = () => {
               <div>
                 <button
                   onClick={handleRemove}
-                  className="px-5 bg-green-200 block mx-auto"
+                  className="px-5 w-52 h-12 bg-green-200 block mx-auto"
                 >
-                  Deleted All favorites
+                  Deleted Carts item
                 </button>
 
                 {/* <h1>Total price : {totalPrice}</h1> */}
               </div>
             )}
             <div className="grid grid-cols-3 gap-7">
-              {cartItem?.map((item) => (
+                {
+                    see ?
+                    cartItem?.map((item) => (
                 <MyCard key={item.id} phone={item}></MyCard>
-              ))}
+              ))
+              :
+              cartItem?.slice(0,4).map((item) => (
+                <MyCard key={item.id} phone={item}></MyCard>
+              ))
+                
+            }
+
+              
             </div>
           </div>
         )}
+        <button
+          onClick={() => setSee(!see)}
+          className="px-5 w-52 h-12 bg-green-200 block mx-auto"
+        >{
+            see ? "see less":"see more..."
+        }
+          
+        </button>
       </div>
     );
 };
